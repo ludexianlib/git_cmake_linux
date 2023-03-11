909e28f7b43f6024026acb3c0e10292cae77cf67
Luu <ludexian@126.com>
2023-03-11 10:24:58 +0800

    sub

--- main.cpp
+++ main.cpp
@@ -1,9 +1,17 @@
 #include <iostream>
 
+int sub(int x, int y)
+{
+    return x - y;
+}
+
 int main(int argc, char* argv[])
 {
     std::cout << "test!" << std::endl;
     std::cout << "test!" << std::endl;
     std::cout << "test!" << std::endl;
+    std::cout << "sub: " << sub(9, 19) << std::endl;
+    std::cout << "sub: " << sub(19, 44) << std::endl;
+    
     return 0;
 }
