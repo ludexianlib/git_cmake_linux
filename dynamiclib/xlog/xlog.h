eeb1ec1a46fe7916b732fae87a79a16a54fd0db0
ldx <ludexian@12.com>
2023-03-11 12:58:01 +0800

    区分windows和linux下动态库导出

--- dynamiclib/xlog/xlog.h
+++ dynamiclib/xlog/xlog.h
@@ -1,7 +1,18 @@
 #ifndef XLOG_H
 #define XLOG_H
 
+#ifdef xlog_EXPORTS
+	#define XLOG_API __declspec(dllexport)
+#else
+	#define XLOG_API __declspec(dllimport)
+
+#endif
+
+#ifdef WIN32
+class XLOG_API XLog
+#else __linux__
 class XLog
+#endif
 {
 public:
 	XLog();
