714ca7ac0cc603e26322cbf23407b42c6ae32260
ldx <ludexian@12.com>
2023-03-11 10:24:00 +0800

    add

--- main.cpp
+++ main.cpp
@@ -1,9 +1,16 @@
 #include <iostream>
 
+int add(int x, int y)
+{
+	return x + y;
+}
+
 int main(int argc, char* argv[])
 {
     std::cout << "test!" << std::endl;
     std::cout << "test!" << std::endl;
-    std::cout << "test!" << std::endl;
+    std::cout << "test1!" << std::endl;
+    std::cout << "add: " < add(3, 5) << std::endl;
+    std::cout << "add: " < add(5, 10) << std::endl;
     return 0;
 }
