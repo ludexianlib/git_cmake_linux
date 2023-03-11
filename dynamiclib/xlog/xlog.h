4f65ce1ec193af5788fa688b1a021129aa71dd58
ldx <ludexian@12.com>
2023-03-11 13:05:24 +0800

    分区windows和linux作法

--- dynamiclib/xlog/xlog.h
+++ dynamiclib/xlog/xlog.h
@@ -1,18 +1,16 @@
 #ifndef XLOG_H
 #define XLOG_H
 
+#ifdef WIN32
 #ifdef xlog_EXPORTS
 	#define XLOG_API __declspec(dllexport)
 #else
 	#define XLOG_API __declspec(dllimport)
-
+#else
+	#define XLOG_API
 #endif
 
-#ifdef WIN32
 class XLOG_API XLog
-#else __linux__
-class XLog
-#endif
 {
 public:
 	XLog();
