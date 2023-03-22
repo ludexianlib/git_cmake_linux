285658afb1873c8f9f872586df8e29ae2ddeccd2
ludexian <13580092832>
2023-03-22 16:20:05 +0800

    结构体地址偏移获取数据

---
+++ modern_c++/knowledge/main.cpp
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <iostream>
+#include <string>
+
+using uchar = unsigned char;
+using ucharptr = unsigned char *;
+
+struct M
+{
+    int x;
+    float y;
+    char d[256] = { 0 };
+};
+
+int main()
+{
+    // 通过结构体地址偏移获取数据
+    M m;
+    m.x = 1;
+    m.y = 1.23456f;
+    strcpy_s(m.d, "test");
+
+    int rx = *(int*)((ucharptr)&m);
+    float ry = *(float*)((ucharptr)&m + 4);
+    
+    char rd[256] = { 0 };
+    for (int i = 0; i < 256; i++)
+        rd[i] = *(char*)((ucharptr)&m + 4 + 4 + i);
+
+    std::cout << rx << std::endl;
+    std::cout << ry << std::endl;
+    std::cout << rd << std::endl;
+
+    return 0;
+}
