7bc51f89d57e25a30b61ea06aef9692916df9797
ludexian <ludexian@126.com>
2023-04-01 19:58:07 +0800

    effective C++

--- modern_c++/effective_c++/main.cpp
+++ modern_c++/effective_c++/main.cpp
@@ -1,3 +1,6 @@
+#include <iostream>
+using namespace std;
+
 // 1.显式禁用拷贝或复制操作
 class ProhibitConstructor
 {
@@ -14,3 +17,15 @@ public:
     virtual ~BaseClass() {}
     virtual void CannotOverride() final; // final修饰该函数无法override
 };
+
+// 3.不要在基类构造函数调用虚函数
+// 子类虚函数可能退化为基类
+class Base
+{
+public:
+    Base() { Init(); }  // warning
+    ~Base() { cout << "Release!" << endl; }
+    virtual void Init(){ cout << "Base P." << endl; }
+};
+
+// 4.operator=时以引用返回*this，避免链式操作出现问题
