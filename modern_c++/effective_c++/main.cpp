5ce885d0bba7bfe6554972abfa578427ca4af598
ludexian <ludexian@126.com>
2023-04-02 13:50:18 +0800

    pthread mutex

--- modern_c++/effective_c++/main.cpp
+++ modern_c++/effective_c++/main.cpp
@@ -29,3 +29,32 @@ public:
 };
 
 // 4.operator=时以引用返回*this，避免链式操作出现问题
+// 5.operator=自我复制避免资源错误释放
+class OperatorClass
+{
+public:
+    OperatorClass& operator= (OperatorClass& ref)
+    {
+        // 1.warning
+        // if (this == &ref) return *this;
+        // delete iptr;
+        // iptr = new int(*ref.iptr); // maybe error
+        // return *this;
+
+        // 2.correct
+        int* pold = iptr;
+        iptr = new int(*ref.iptr);
+        delete pold;
+        return *this;
+    }
+private:
+    int* iptr;
+};
+
+// 6.在子类构造函数初始化基类
+// ChildClass(ChildClass& ref) : BaseClass(ref) {}
+
+// 7.以对象管理资源，智能指针的思想
+// 8.尽可能pass by reference to const, 而不是pass by value
+// 9.必须返回对象时，尽量使用c++11 std::move()避免临时对象拷贝
+// 10.成员变量设计为private，通过成员函数接口访问
---
+++ os/thread/mutex_linux.cpp
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <pthread.h>
+#include <unistd.h>
+
+int titck_num = 2;
+pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
+
+void* thread_func(void* args)
+{
+    // 上锁
+    pthread_mutex_lock(&lock);
+    int t = titck_num;
+    if (t >= 0)
+    {
+        printf("one titck sold.\n");
+        t--;
+    }
+    else
+    {
+        printf("titck sold out.\n");
+    }
+    titck_num = t;
+    // 解锁
+    pthread_mutex_unlock(&lock);
+
+}
+
+int main()
+{
+
+    pthread_t tid[2];
+    for (int i = 0; i < 2; i++)
+    {
+        pthread_create(tid + i, NULL, thread_func, NULL);
+    }
+    for (int i = 0; i < 2; i++)
+    {
+        pthread_join(tid[i], NULL);
+    }
+    printf("titck number: %d\n", titck_num);
+
+    return 0;
+}
