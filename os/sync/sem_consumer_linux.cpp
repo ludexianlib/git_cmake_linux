8c0d8a0da5894577a7bc7aad555bb1e9a983ac6c
ludexian <ludexian@126.com>
2023-04-02 21:47:35 +0800

    try to add two mutex in driver

--- os/sync/sem_consumer_linux.cpp
+++ os/sync/sem_consumer_linux.cpp
@@ -10,7 +10,8 @@ int buf[K];             // 缓冲区大小
 int in = 0, out = 0;    // 当前访问数组位置
 sem_t empty;            // 当用生产者可用资源
 sem_t full;             // 当前消费者可用资源
-sem_t mutex;            // 互斥资源
+sem_t mutex1;           // 互斥资源
+sem_t mutex2;
 
 void* producer_thread(void* args)
 {
@@ -19,11 +20,11 @@ void* producer_thread(void* args)
     {
         sem_wait(&empty);           // 缓冲区是否已满
 
-        sem_wait(&mutex);           // 互斥资源是否可用
+        sem_wait(&mutex1);           // 互斥资源是否可用
         buf[in] = rand() % 1000;    // 随机生成整数放入缓冲区
-        printf("线程号[%u] 生产: %d\n", pthread_self(), buf[in]);
+        printf("线程号[%lu] 生产: %d\n", pthread_self(), buf[in]);
         in = (in + 1) % K;          // 相当于循环队列
-        sem_post(&mutex);
+        sem_post(&mutex1);
 
         sem_post(&full);            // 缓冲区资源已增加
 
@@ -42,11 +43,11 @@ void* consumer_thread(void* args)
     {
         sem_wait(&full);            // 缓冲区是否为空
 
-        sem_wait(&mutex);           // 互斥资源是否可用
+        sem_wait(&mutex2);           // 互斥资源是否可用
         int get = buf[out];         // 随机生成整数放入缓冲区
-        printf("线程号[%u] 从buf[%d] 获取数据: %d\n", pthread_self(), out, get);
+        printf("线程号[%lu] 从buf[%d] 获取数据: %d\n", pthread_self(), out, get);
         out = (out + 1) % K;        // 相当于循环队列
-        sem_post(&mutex);
+        sem_post(&mutex2);
 
         sem_post(&empty);            // 缓冲区资源已增加
         
@@ -62,7 +63,8 @@ int main()
 {
     sem_init(&empty, 0, K);     // 初始化生产者可用资源
     sem_init(&full, 0, 0);      // 初始化消费者可用资源
-    sem_init(&mutex, 0, 1);     // 互斥资源
+    sem_init(&mutex1, 0, 1);     // 互斥资源
+    sem_init(&mutex2, 0, 1);     // 互斥资源
 
     pthread_t producers[10];
     pthread_t consumers[10];
@@ -79,6 +81,7 @@ int main()
 
     sem_destroy(&empty);
     sem_destroy(&full);
-    sem_destroy(&mutex);
+    sem_destroy(&mutex1);
+    sem_destroy(&mutex2);
     return 0;
 }
