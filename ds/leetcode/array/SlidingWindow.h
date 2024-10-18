95cf84a3c3b0cf64f9de7c9525013e1b121d0c37
ludexian <ludexian@126.com>
2024-10-18 21:15:15 +0800

    feat: 添加滑动窗口

--- ds/leetcode/array/SlidingWindow.h
+++ ds/leetcode/array/SlidingWindow.h
@@ -11,6 +11,31 @@ namespace arr
     class SlidingWindow
     {
     public:
-        
+        /*
+         *brief  \求 >= s的最短连续序列
+         *param  \
+         */
+        int minSubArrayLen(int s, vector<int>& nums)
+        {
+            // 移动快慢指针形成滑动窗口
+            int size = nums.size();
+            int sum = 0;
+            int left = 0;
+            int length = INT32_MAX;
+
+            // 快指针向右移动
+            for (int i = 0; i < size; i++)
+            {
+                sum += nums[i];
+                while (sum >= s)
+                {
+                    // 慢指针向右移动
+                    int len = i - left + 1; // 窗口长度
+                    length = len < length ? len : length;
+                    sum -= nums[left++];
+                }
+            }
+            return length == INT32_MAX ? 0 : length;
+        }
     };
 }
